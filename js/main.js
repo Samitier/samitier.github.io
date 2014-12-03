2d5fd36bb89d3045e38089fe9c1779a8979db716
Blai <blai.samitier@gmail.com>
2014-12-04 00:12:44 +0100

    cube

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+var light = new THREE.PointLight(0xEEEEEE);
+light.position.set(20, 0, 20);
+scene.add(light);
+
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+var texture = THREE.ImageUtils.loadTexture( 'txt.png' );
+var material = new THREE.MeshBasicMaterial( {map: texture } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 1.4;
+render();
+
+function render() {
+   requestAnimationFrame( render );
+
+   cube.rotation.x += 0.01;
+   cube.rotation.y += 0.005;
+
+   renderer.setClearColor( 0xffffff, 1);
+   renderer.render( scene, camera );
+}
