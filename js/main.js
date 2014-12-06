06362157b3f71c79dc4b5cb983fbf81c3243a9e6
Blai <blai.samitier@gmail.com>
2014-12-06 15:26:49 +0100

    added checking for browser features

--- js/main.js
+++ js/main.js
@@ -3,31 +3,34 @@ var cube;
 var rotx, roty, mousex, mousey, mouseDown;
 
 function init() {
-   scene = new THREE.Scene();
-   camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-   renderer = new THREE.WebGLRenderer();
-   renderer.setSize( window.innerWidth, window.innerHeight );
-   document.body.appendChild( renderer.domElement );
+   if(Modernizr.webgl && Modernizr.canvas) {
+      scene = new THREE.Scene();
+      camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+      renderer = new THREE.WebGLRenderer();
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.body.appendChild( renderer.domElement );
 
-   var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-   var texture = THREE.ImageUtils.loadTexture( 'txt.png' );
-   var material = new THREE.MeshBasicMaterial( {map: texture } );
-   cube = new THREE.Mesh( geometry, material );
-   scene.add( cube );
-   camera.position.z = 1.4;
+      var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+      var texture = THREE.ImageUtils.loadTexture( 'txt.png' );
+      var material = new THREE.MeshBasicMaterial( {map: texture } );
+      cube = new THREE.Mesh( geometry, material );
+      scene.add( cube );
+      camera.position.z = 1.4;
 
-   rotx = roty =  mousex= mousey=0;
-   mouseDown = false;
+      rotx = roty =  mousex= mousey=0;
+      mouseDown = false;
 
-   renderer.domElement.addEventListener( 'mousemove', onMouseMove );
-   renderer.domElement.addEventListener( 'mousedown', onMouseDown );
-   renderer.domElement.addEventListener( 'mouseup', onMouseUp );
-
-   renderer.domElement.addEventListener( 'touchstart', onTouchStart);
-   renderer.domElement.addEventListener( 'touchmove', onTouchMove);
-
-
-   render();
+      if (Modernizr.touch) {
+         renderer.domElement.addEventListener( 'touchstart', onTouchStart);
+         renderer.domElement.addEventListener( 'touchmove', onTouchMove);
+      } else {
+         renderer.domElement.addEventListener( 'mousemove', onMouseMove );
+         renderer.domElement.addEventListener( 'mousedown', onMouseDown );
+         renderer.domElement.addEventListener( 'mouseup', onMouseUp );
+      }
+      render();
+   }
+   else alert("Your browser don't support WebGl! :(");
 }
 
 function render() {
@@ -40,6 +43,7 @@ function render() {
    renderer.render( scene, camera );
 }
 
+///EVENTS///
 function onMouseMove( event ) {
    if(mouseDown) {
       roty += (event.x-mousex)*0.01;
@@ -48,23 +52,18 @@ function onMouseMove( event ) {
       mousey = event.y;
    }
 }
-
 function onMouseDown( event ) {
    mousex = event.x;
    mousey = event.y;
    mouseDown = true;
 }
-
 function onMouseUp( event ) {
    mouseDown = false;
 }
-
 function onTouchStart (event) {
-   console.dir(event);
    mousex = event.touches[0].screenX;
    mousey = event.touches[0].screenY;
 }
-
 function onTouchMove (event) {
    roty += (event.touches[0].screenX-mousex)*0.01;
    rotx += (event.touches[0].screenY-mousey)*0.01;
