258de1c8355b0ac7ef730b28d85e15edcbea1a0c
Blai <blai.samitier@gmail.com>
2014-12-25 22:05:38 +0100

    contact page, projects page

--- js/pages.js
+++ js/pages.js
@@ -2,14 +2,14 @@ var uv     = { main : [new THREE.Vector2(0, .875), new THREE.Vector2(.125, .875)
                website : [new THREE.Vector2(.125, .875), new THREE.Vector2(.25, .875), new THREE.Vector2(.25, 1),new THREE.Vector2(.125, 1)],
                contact : [new THREE.Vector2(.250, .875), new THREE.Vector2(.375, .875), new THREE.Vector2(.375, 1),new THREE.Vector2(.250, 1)],
                me : [new THREE.Vector2(.375, .875), new THREE.Vector2(.500, .875), new THREE.Vector2(.500, 1),new THREE.Vector2(.375, 1)],
-               projects : [new THREE.Vector2(.375, .875), new THREE.Vector2(.500, .875), new THREE.Vector2(.500, 1),new THREE.Vector2(.375, 1)]
+               projects : [new THREE.Vector2(.5, .875), new THREE.Vector2(.625, .875), new THREE.Vector2(.625, 1),new THREE.Vector2(.5, 1)]
 };
 
 var bkgMain = THREE.ImageUtils.loadTexture( 'img/bkg/bkgmain.png' );
 var bkgWebsite = THREE.ImageUtils.loadTexture( 'img/bkg/bkgweb.png');
-var bkgContact = THREE.ImageUtils.loadTexture( 'img/bkg/bkgcontact2.png');
+var bkgContact = THREE.ImageUtils.loadTexture( 'img/bkg/bkgcontact.png');
 var bkgProva1 = THREE.ImageUtils.loadTexture( 'img/bkg/bkg22.png');
-var bkgProva2 = THREE.ImageUtils.loadTexture( 'img/bkg/bkg3.jpg');
+var bkgProva2 = THREE.ImageUtils.loadTexture( 'img/bkg/bkg3.png');
 
 var navs = {   main:    {top : "projects",bottom : "me",   left : "website", right: "contact", front: "main",    background:bkgMain},
                website: {top : "main",    bottom : "main", left : "main",    right: "main",     front: "website", background:bkgWebsite},
