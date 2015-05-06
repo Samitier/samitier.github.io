386742046a4ecfae00b205ed730dfa214b8917c9
Blai Samitier <blai.samitier@gmail.com>
2015-05-06 13:28:38 +0200

    added projects navigation

--- js/pages.js
+++ js/pages.js
@@ -5,7 +5,22 @@ var uv     = { main : [new THREE.Vector2(0, .875), new THREE.Vector2(.125, .875)
                projects : [new THREE.Vector2(.5, .875), new THREE.Vector2(.625, .875), new THREE.Vector2(.625, 1),new THREE.Vector2(.5, 1)],
                education : [new THREE.Vector2(.6251, .875), new THREE.Vector2(.7499, .875), new THREE.Vector2(.7499, 1),new THREE.Vector2(.6251, 1)],
                skills : [new THREE.Vector2(.75, .875), new THREE.Vector2(.875, .875), new THREE.Vector2(.875, 1),new THREE.Vector2(.75, 1)],
-               motivations : [new THREE.Vector2(.875, .875), new THREE.Vector2(1, .875), new THREE.Vector2(1, 1),new THREE.Vector2(.875, 1)]
+               motivations : [new THREE.Vector2(.875, .875), new THREE.Vector2(1, .875), new THREE.Vector2(1, 1),new THREE.Vector2(.875, 1)],
+
+               vj1 : [new THREE.Vector2(0, .75), new THREE.Vector2(.125, .75), new THREE.Vector2(.125, .875),new THREE.Vector2(0, .875)],
+               vj2 : [new THREE.Vector2(.125, .75), new THREE.Vector2(.25, .75), new THREE.Vector2(.25, .875),new THREE.Vector2(.125, .875)],
+               vj3 : [new THREE.Vector2(.250, .75), new THREE.Vector2(.375, .75), new THREE.Vector2(.375, .875),new THREE.Vector2(.250, .875)],
+               vj4 : [new THREE.Vector2(.375, .75), new THREE.Vector2(.500, .75), new THREE.Vector2(.500, .875),new THREE.Vector2(.375, .875)],
+               vj5 : [new THREE.Vector2(.5, .75), new THREE.Vector2(.625, .75), new THREE.Vector2(.625, .875),new THREE.Vector2(.5, .875)],
+
+               w1 : [new THREE.Vector2(0, .625), new THREE.Vector2(.125, .625), new THREE.Vector2(.125, .75),new THREE.Vector2(0, .75)],
+               w2 : [new THREE.Vector2(.125, .625), new THREE.Vector2(.25, .625), new THREE.Vector2(.25, .75),new THREE.Vector2(.125, .75)],
+               w3 : [new THREE.Vector2(.250, .625), new THREE.Vector2(.375, .625), new THREE.Vector2(.375, .75),new THREE.Vector2(.250, .75)],
+
+               a1 : [new THREE.Vector2(0, .500), new THREE.Vector2(.125, .500), new THREE.Vector2(.125, .625),new THREE.Vector2(0, .625)],
+               a2 : [new THREE.Vector2(.125, .500), new THREE.Vector2(.25, .500), new THREE.Vector2(.25, .625),new THREE.Vector2(.125, .625)]
+
+
 
 };
 
@@ -29,8 +44,21 @@ var navs = {   main:      {top : "projects",bottom : "me",   left : "website", r
                website:   {top : "main",    bottom : "main", left : "main",    right: "main",     front: "website", background:bkgWebsite},
                contact:   {top : "main",    bottom : "main", left : "main",    right: "main",     front: "contact", background:bkgContact},
                me:        {top : "main",    bottom : "skills", left : "education",    right: "motivations",     front: "me",      background:bkgAboutMe},
-               projects:  {top : "main",    bottom : "main", left : "main",    right:"main",      front: "projects",background:bkgProjects},
+               projects:  {top : "w1",    bottom : "main", left : "a1",    right:"vj1",      front: "projects",background:bkgProjects},
                education: {top : "main",    bottom : "main", left : "main",    right:"me",      front: "education",background:bkgEducation},
                skills:    {top : "me",    bottom : "main", left : "main",    right:"main",      front: "skills",   background:bkgSkills},
-               motivations:{top : "main",   bottom : "main", left : "me",    right:"main",      front: "motivations",  background:bkgMotivations}
+               motivations:{top : "main",   bottom : "main", left : "me",    right:"main",      front: "motivations",  background:bkgMotivations},
+
+               vj1: {top : "vj2",   bottom : "vj5", left : "projects",    right:"main",      front: "vj1",  background:bkgMain},
+               vj2: {top : "vj3",   bottom : "vj1", left : "projects",    right:"main",      front: "vj2",  background:bkgMain},
+               vj3: {top : "vj4",   bottom : "vj2", left : "projects",    right:"main",      front: "vj3",  background:bkgMain},
+               vj4: {top : "vj5",   bottom : "vj3", left : "projects",    right:"main",      front: "vj4",  background:bkgMain},
+               vj5: {top : "vj1",   bottom : "vj4", left : "projects",    right:"main",      front: "vj5",  background:bkgMain},
+
+               w1: {top : "main",   bottom : "projects", left : "w3",    right:"w2",      front: "w1",  background:bkgMain},
+               w2: {top : "main",   bottom : "projects", left : "w1",    right:"w3",      front: "w2",  background:bkgMain},
+               w3: {top : "main",   bottom : "projects", left : "w2",    right:"w1",      front: "w3",  background:bkgMain},
+
+               a1: {top : "a2",   bottom : "a2", left : "main",    right:"projects",      front: "a1",  background:bkgMain},
+               a2: {top : "a1",   bottom : "a1", left : "main",    right:"projects",      front: "a2",  background:bkgMain},
 }
