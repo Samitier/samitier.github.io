93a837326b1e380ac5e4466fd4c4ba39f99ef910
Blai Samitier <blai@samitier.me>
2017-08-05 11:41:20 +0200

    logic completed (version compatibility problem)

--- src/js/cube-scene.js
+++ src/js/cube-scene.js
@@ -1,6 +1,6 @@
 import {
 	BoxGeometry, Mesh, MeshBasicMaterial,
-	Scene, PerspectiveCamera
+	Scene, PerspectiveCamera, Vector2
 } from "three"
 
 export default class CubeScene {
@@ -9,6 +9,8 @@ export default class CubeScene {
 		this.scene = new Scene()
 		this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5)
 		this.camera.position.z = 1.4
+        this.textureWidth =  map.image.width
+        this.textureHeight =  map.image.height
 		this.cube = new Mesh(
 			new BoxGeometry(1, 1, 1), 
 			new MeshBasicMaterial({ 
@@ -31,39 +33,30 @@ export default class CubeScene {
 		return this.cube.rotation
 	}
 
-	setCubeUVs(page, site) {
-		let facesPerWidth = site.texture.image.width / site.faceSize
-
-		// Top
-		let topPosition = site.pages[page.navigation[0]].position
-		this.cube.geometry.faceVertexUvs[0][4] = [uv[page.top][3], uv[page.top][0], uv[page.top][2]];
-		this.cube.geometry.faceVertexUvs[0][5] = [uv[page.top][0], uv[page.top][1], uv[page.top][2]];
-		
-		// Right
-		this.cube.geometry.faceVertexUvs[0][0] = [uv[page.right][3], uv[page.right][0], uv[page.right][2]];
-		this.cube.geometry.faceVertexUvs[0][1] = [uv[page.right][3], uv[page.right][0], uv[page.right][2]];
-
-		var uv     = { main : [new Vector2(0, .75), new Vector2(.125, .75), new Vector2(.125, 1),new Vector2(0, 1)],
-
-cube.geometry.faceVertexUvs[0][0] = [uv[page.right][3], uv[page.right][0], uv[page.right][2]];
-	cube.geometry.faceVertexUvs[0][1] = [uv[page.right][0], uv[page.right][1], uv[page.right][2]];
-	cube.geometry.faceVertexUvs[0][2] = [uv[page.left][3], uv[page.left][0], uv[page.left][2]];
-	cube.geometry.faceVertexUvs[0][3] = [uv[page.left][0], uv[page.left][1], uv[page.left][2]];
-	cube.geometry.faceVertexUvs[0][6] = [uv[page.bottom][3], uv[page.bottom][0], uv[page.bottom][2]];
-	cube.geometry.faceVertexUvs[0][7] = [uv[page.bottom][0], uv[page.bottom][1], uv[page.bottom][2]];
-	cube.geometry.faceVertexUvs[0][8] = [uv[page.front][3], uv[page.front][0], uv[page.front][2]];
-	cube.geometry.faceVertexUvs[0][9] = [uv[page.front][0], uv[page.front][1], uv[page.front][2]];
-
-		for (let i = 0; i < 10; ++i) {
-			let isOdd = i%2 != 0,
-				side = i <= 1 ? "right" : i <= 3 ? "left" : i <= 5 ? "top" : i <= 7 ? "bottom" : "front" 
-			this.cube.geometry.faceVertexUvs[0][i] = [
-				uv[page[side]][isOdd ? 0 : 3],
-				uv[page[side]][isOdd ? 1 : 0],
-				uv[page[side]][2]
-			]
-		}
-		this.cube.geometry.faceVertexUvs[0][10] = [0, 0, 0]
+	setCubeUVs(pageName, site) {
+		let page = site.pages[pageName],
+            faceWidthRatio = 1/(this.textureWidth / site.faceSize),
+            faceHeightRatio = 1/(this.textureHeight / site.faceSize),
+            vertexUvs = [ 4, 0, 6, 2, 8 ]
+        
+        for (let i = 0; i <= page.navigation.length; ++i) {
+            let hPosition = i < page.navigation.length 
+                            ? site.pages[page.navigation[i] || "home"].position : page.position,
+                vPosition = Math.floor(hPosition/(this.textureWidth / site.faceSize)),
+                topx = hPosition * faceWidthRatio,
+                bottomx = (hPosition + 1) * faceWidthRatio,
+                topy = 1 - vPosition * faceHeightRatio,
+                bottomy = 1 - ((vPosition + 1) * faceHeightRatio),
+                uvs = [
+                    new Vector2(topx, bottomy),
+                    new Vector2(bottomx, bottomy),
+                    new Vector2(bottomx, topy),
+                    new Vector2(topx, topy),
+                ]
+            this.cube.geometry.faceVertexUvs[0][vertexUvs[i]] = [uvs[3], uvs[0], uvs[2]]
+            this.cube.geometry.faceVertexUvs[0][vertexUvs[i] + 1] = [uvs[0], uvs[1], uvs[2]]
+        }
+        this.cube.geometry.faceVertexUvs[0][10] = [0, 0, 0]
 		this.cube.geometry.faceVertexUvs[0][11] = [0, 0, 0]
 		this.cube.geometry.uvsNeedUpdate = true
 	}
--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,4 @@
-import {
-	Raycaster, Vector2, PlaneGeometry, Mesh, MeshBasicMaterial,
-	Scene, OrthographicCamera, PerspectiveCamera, WebGLRenderer,
-	BoxGeometry, ImageUtils, DefaultLoadingManager, TextureLoader
-} from "three"
+import { WebGLRenderer, DefaultLoadingManager, TextureLoader } from "three"
 
 import BackgroundScene from "./background-scene"
 import CubeScene from "./cube-scene"
@@ -22,7 +18,7 @@ class Main {
 		let loader = new TextureLoader(),
 			path = "/assets"
 		this.cubeTexture = loader.load(path + site.texture)
-		this.backgroundTextures = site.backgrounds.map(b => loader.load(path + site.texture))
+		this.backgroundTextures = site.backgrounds.map(b => loader.load(path + b))
 		DefaultLoadingManager.onProgress = ( item, loaded, total ) => {
 			let percentile = Math.round(loaded/total*100)
 			document.getElementById("loading-text").innerHTML = `Loading: ${percentile}%`
@@ -37,7 +33,7 @@ class Main {
 		// Scenes
 		this.backgroundScene = new BackgroundScene(this.backgroundTextures[0])
 		this.cubeScene = new CubeScene(this.cubeTexture)
-		this.setPage(0)
+		this.setPage("home")
 
 		// Renderer
 		this.renderer = new WebGLRenderer()
@@ -92,28 +88,24 @@ class Main {
 		// If the user is moving the cube we set the new transitioning background
 		else {
 			let cubeRotation = this.cubeScene.getCubeRotation(),
-				rotationFace = ""
-			if (rotation.x > 0 && cubeRotation.x <= 0) rotationFace = "top"
-			else if (rotation.x < 0 && cubeRotation.x >= 0) rotationFace = "bottom"
-			if (rotation.y > 0 && cubeRotation.y <= 0) rotationFace = "left"
-			else if (rotation.y < 0 && cubeRotation.y >= 0) rotationFace = "right"
-			if (rotationFace) {
-				this.backgroundScene.setBackgroundMap(
-					1, navs[navs[this.curPage][rotationFace]].background
-				)
+				rotationFace = -1
+			if (rotation.x > 0 && cubeRotation.x <= 0) rotationFace = 0
+			else if (rotation.x < 0 && cubeRotation.x >= 0) rotationFace = 2
+			if (rotation.y > 0 && cubeRotation.y <= 0) rotationFace = 3
+			else if (rotation.y < 0 && cubeRotation.y >= 0) rotationFace = 1
+			if (rotationFace >= 0) {
+                let navigatedPage = site.pages[ site.pages[this.currentPage].navigation[rotationFace] || "home" ]
+                this.backgroundScene.setBackgroundMap( 1, this.backgroundTextures[ navigatedPage.background ])
 			}
 		}
 		// If the user completely rotates the cube, the navigation will happen:
 		// and the face textures will change 
 		if (Math.abs(rotation.x) > MAX_ROTATION || Math.abs(rotation.y) > MAX_ROTATION) {
-			let dir = ""
+			let direction = rotation.y > 0 ? 3 : 1
 			if (Math.abs(rotation.x) > MAX_ROTATION) {
-				dir = rotation.x > 0 ? "top" : "bottom"
+				direction = rotation.x > 0 ? 0 : 2
 			}
-			else if(Math.abs(rotation.y) > MAX_ROTATION) {
-				dir = rotation.y > 0 ? "left" : "right"
-			}
-			this.setPage(navs[navs[this.curPage][dir]])
+			this.setPage(site.pages[this.currentPage].navigation[direction])
 			rotation.x = rotation.y = 0
 		}
 		this.cubeScene.setCubeRotation("x", rotation.x)
@@ -121,10 +113,13 @@ class Main {
 		this.eventManager.setRotation(rotation)
 	}
 
-	setPage(pagePosition) {
-		this.currentPage = pagePosition
-		this.cubeScene.setCubeUVs(this.getCurrentPage, site)
-		this.backgroundScene.setBackgroundMap(0, page["background"])
+	setPage(pageName) {
+		this.currentPage = pageName || "home"
+		this.cubeScene.setCubeUVs(this.currentPage, site)
+        this.backgroundScene.setBackgroundMap(
+            0, 
+            this.backgroundTextures[ site.pages[this.currentPage].background ]
+        )
 	}
 
 	onResize() {
@@ -134,90 +129,9 @@ class Main {
 		this.cubeScene.setCameraAspectRatio(window.innerWidth / window.innerHeight)
 		this.renderer.setSize(window.innerWidth, window.innerHeight)
 	}
-
-	getCurrentPage() {
-		return site.pages.find(p => p.position == this.currentPage)
-	}
 }
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-var uv     = { main : [new Vector2(0, .75), new Vector2(.125, .75), new Vector2(.125, 1),new Vector2(0, 1)],
-               website : [new Vector2(.125, .75), new Vector2(.2499, .75), new Vector2(.2499, 1),new Vector2(.125, 1)],
-               contact : [new Vector2(.250, .75), new Vector2(.375, .75), new Vector2(.375, 1),new Vector2(.250, 1)],
-               me : [new Vector2(.3751, .75), new Vector2(.500, .75), new Vector2(.500, 1),new Vector2(.3751, 1)],
-               projects : [new Vector2(.5, .75), new Vector2(.625, .75), new Vector2(.625, 1),new Vector2(.5, 1)],
-               education : [new Vector2(.6251, .75), new Vector2(.7499, .75), new Vector2(.7499, 1),new Vector2(.6251, 1)],
-               skills : [new Vector2(.75, .75), new Vector2(.875, .75), new Vector2(.875, 1),new Vector2(.75, 1)],
-               motivations : [new Vector2(.8751, .75), new Vector2(1, .75), new Vector2(1, 1),new Vector2(.8751, 1)],
-
-               vj1 : [new Vector2(0, .5), new Vector2(.125, .5), new Vector2(.125, .75),new Vector2(0, .75)],
-               vj2 : [new Vector2(.125, .5), new Vector2(.25, .5), new Vector2(.25, .75),new Vector2(.125, .75)],
-               vj3 : [new Vector2(.250, .5), new Vector2(.375, .5), new Vector2(.375, .75),new Vector2(.250, .75)],
-               vj4 : [new Vector2(.375, .5), new Vector2(.500, .5), new Vector2(.500, .75),new Vector2(.375, .75)],
-               vj5 : [new Vector2(.5, .5), new Vector2(.625, .5), new Vector2(.625, .75),new Vector2(.5, .75)],
-
-               w1 : [new Vector2(0, .25), new Vector2(.125, .25), new Vector2(.125, .5),new Vector2(0, .5)],
-               w2 : [new Vector2(.125, .25), new Vector2(.25, .25), new Vector2(.25, .5),new Vector2(.125, .5)],
-               w3 : [new Vector2(.250, .25), new Vector2(.375, .25), new Vector2(.375, .5),new Vector2(.250, .5)],
-
-               a1 : [new Vector2(0, .0), new Vector2(.1249, .0), new Vector2(.1249, .25),new Vector2(0, .25)],
-               a2 : [new Vector2(.125, .0), new Vector2(.25, .0), new Vector2(.25, .25),new Vector2(.125, .25)]
-
-
-
-};
-
-var pages = ImageUtils.loadTexture( '/assets/pages/pages.png' );console.log(pages);
-
-var bkgMain = ImageUtils.loadTexture( '/assets/bkg/bkgmain.jpg' );
-var bkgWebsite = ImageUtils.loadTexture( '/assets/bkg/bkgweb.jpg');
-var bkgContact = ImageUtils.loadTexture( '/assets/bkg/bkgcontact.jpg');
-var bkgAboutMe = ImageUtils.loadTexture( '/assets/bkg/bkgAboutMe.jpg');
-var bkgProjects = ImageUtils.loadTexture( '/assets/bkg/bkgProjects.jpg');
-var bkgMotivations = ImageUtils.loadTexture( '/assets/bkg/bkgMotivations.jpg');
-var bkgSkills = ImageUtils.loadTexture( '/assets/bkg/bkgSkills.jpg');
-var bkgEducation = ImageUtils.loadTexture( '/assets/bkg/bkgEducation.jpg');
-var bkgVj = ImageUtils.loadTexture( '/assets/bkg/bkgVj.png');
-var bkgWb = ImageUtils.loadTexture( '/assets/bkg/bkgWb.jpg');
-var bkgAndroid = ImageUtils.loadTexture( '/assets/bkg/bkgAndroid.jpg');
-
-DefaultLoadingManager.onProgress = function ( item, loaded, total ) {
-    document.getElementById("loading-text").innerHTML = "Loading: " + Math.round(loaded/total*100) + "%";
-    if(loaded==total) {
-        document.getElementById("loading-div").style.display = 'none';
-    }
-};
-
-var navs = {   main:      {top : "projects",bottom : "me",   left : "website", right: "contact", front: "main",    background:bkgMain},
-               website:   {top : "main",    bottom : "main", left : "main",    right: "main",     front: "website", background:bkgWebsite},
-               contact:   {top : "main",    bottom : "main", left : "main",    right: "main",     front: "contact", background:bkgContact},
-               me:        {top : "main",    bottom : "skills", left : "education",    right: "motivations",     front: "me",      background:bkgAboutMe},
-               projects:  {top : "w1",    bottom : "main", left : "a1",    right:"vj1",      front: "projects",background:bkgProjects},
-               education: {top : "main",    bottom : "main", left : "main",    right:"me",      front: "education",background:bkgEducation},
-               skills:    {top : "me",    bottom : "main", left : "main",    right:"main",      front: "skills",   background:bkgSkills},
-               motivations:{top : "main",   bottom : "main", left : "me",    right:"main",      front: "motivations",  background:bkgMotivations},
-
-               vj1: {top : "vj2",   bottom : "vj5", left : "projects",    right:"main",      front: "vj1",  background:bkgVj, buttons:[{height:0.073, width:0.34, x:0.08, y:-0.288, link:"https://www.youtube.com/watch?v=tVEEdebiVG8"}]},
-               vj2: {top : "vj3",   bottom : "vj1", left : "projects",    right:"main",      front: "vj2",  background:bkgVj, buttons:[{height:0.073, width:0.34, x:0.08, y:-0.288, link:"https://www.youtube.com/watch?v=Vp_X0DgzwpI"},{height:0.073, width:0.34, x:-0.42, y:-0.288, link:"http://www.kongregate.com/games/lepka/mongo-the-shurmanos"}]},
-               vj3: {top : "vj4",   bottom : "vj2", left : "projects",    right:"main",      front: "vj3",  background:bkgVj, buttons:[{height:0.073, width:0.34, x:0.08, y:-0.288, link:"https://www.youtube.com/watch?v=U5m4ixkeEX0"},{height:0.073, width:0.34, x:-0.42, y:-0.288, link:"http://samitier.github.io/Snow-Bros-Reborn/"}]},
-               vj4: {top : "vj5",   bottom : "vj3", left : "projects",    right:"main",      front: "vj4",  background:bkgVj, buttons:[{height:0.073, width:0.34, x:0.08, y:-0.288, link:"https://www.youtube.com/watch?v=5jJCd3k_l98"},{height:0.073, width:0.34, x:-0.42, y:-0.288, link:"https://github.com/albarralnunez/TowerDefense"}]},
-               vj5: {top : "vj1",   bottom : "vj4", left : "projects",    right:"main",      front: "vj5",  background:bkgVj, buttons:[{height:0.073, width:0.34, x:0.08, y:-0.288, link:"https://www.youtube.com/channel/UCjI7xjQrVNpVbXngJEimwSQ"}]},
-
-               w1: {top : "main",   bottom : "projects", left : "w3",    right:"w2",      front: "w1",  background:bkgWb, buttons:[{height:0.073, width:0.34, x:-0.178, y:-0.342, link:"http://dhis2-hii.asfes.org/"}]},
-               w2: {top : "main",   bottom : "projects", left : "w1",    right:"w3",      front: "w2",  background:bkgWb, buttons:[{height:0.073, width:0.34, x:0.08, y:-0.288, link:"https://github.com/ferranmarlet/ticketForAll"},{height:0.073, width:0.34, x:-0.42, y:-0.288, link:"http://ticketforall.herokuapp.com/"}]},
-               w3: {top : "main",   bottom : "projects", left : "w2",    right:"w1",      front: "w3",  background:bkgWb},
-
-               a1: {top : "a2",   bottom : "a2", left : "main",    right:"projects",      front: "a1",  background:bkgMain, buttons:[{height:0.073, width:0.34, x:-0.178, y:-0.342, link:"https://github.com/Samitier/Share-the-Sound"}]},
-               a2: {top : "a1",   bottom : "a1", left : "main",    right:"projects",      front: "a2",  background:bkgAndroid},
-}
 new Main()
-
-///////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 
 
